09c9e620f793dfcc83d20b29a0c47fe816eaaee3
Miro Adamy <miro.adamy@pivotree.com>
2022-08-22 21:11:42 +0200

    fix: added the semantic versioning config

---
+++ react-app/release.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    branches: "main",
+    repositoryUrl: "https://github.com/miroadamy-practice/github-actions-demo-1",
+    plugins: [
+        '@semantic-release/commit-analyzer', 
+        '@semantic-release/release-notes-generator', 
+        '@semantic-release/npm', 
+        '@semantic-release/github'
+    ]
+
+}
