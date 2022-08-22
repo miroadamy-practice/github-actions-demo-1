e184abe28bfd1d6db3b593b943420ee205567a48
Miro Adamy <miro.adamy@pivotree.com>
2022-08-22 22:46:41 +0200

    feat: upload artifacts

--- react-app/release.config.js
+++ react-app/release.config.js
@@ -4,7 +4,14 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/npm',
-    '@semantic-release/github',
+    [
+      '@semantic-release/github',
+      {
+        assets: [
+          { path: 'build.zip', label: 'Build' },
+          { path: 'coverage.zip', label: 'Coverage' },
+        ],
+      },
+    ],
   ],
 };
