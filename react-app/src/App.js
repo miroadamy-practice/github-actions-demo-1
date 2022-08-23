408c0b30442882b4506b9ac2edcb117bad501dae
Miro Adamy <miro.adamy@pivotree.com>
2022-08-23 23:58:19 +0200

    fix: format OK again, we will merge

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() { return (
+function App() {
+  return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
