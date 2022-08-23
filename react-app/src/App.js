e4e9af14a9035d79957929b6807595ce0356e8f4
Miro Adamy <miro.adamy@pivotree.com>
2022-08-23 23:46:06 +0200

    fix: New workflow, updated releas WF, broke the app again

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,8 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+function App() { return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
