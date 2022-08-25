80caf3a072b714ebcae5b115b63ae8aa074ae1ce
Miro Adamy <miro.adamy@pivotree.com>
2022-08-25 23:30:38 +0200

    First version of hello

---
+++ .github/actions/hello/index.js
@@ -0,0 +1,17 @@
+const core = require('@actions/core');
+const githb = require('@actions/github');
+
+// lets simulate an error => on GH action, it will seem to be successful
+try {
+    // throw( new Error('Some simulated error'));
+
+    const name = core.getInput('whom-to-greet');
+    console.log(`Hello, ${name}`);
+
+    const time = new Date();
+    core.setOutput("time", time.toTimeString());
+
+    console.log.log(JSON.stringify(github, null, '\t'));
+} catch (error) {}
+    core.setFailed('I failed');
+}
