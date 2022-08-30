78a12f3642994da4f55e918eb17d54d634c22c7c
Miro Adamy <miro.adamy@pivotree.com>
2022-08-30 22:31:58 +0200

    fix: recompile

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -428,13 +428,27 @@ const github = __webpack_require__(469);
 try {
     // throw( new Error('Some simulated error'));
 
+    core.debug('Debug message');
+    core.warning('Warning message');
+
+    core.error('Error Message - it will NOT cause the fail');
+
+
     const name = core.getInput('whom-to-greet');
+
+    core.setSecret(name);   // this will cause the name not being displayed
     console.log(`Hello, ${name}`);
 
     const time = new Date();
     core.setOutput("time", time.toTimeString());
 
+    core.startGroup('Logging Github object');
     console.log(JSON.stringify(github, null, '\t'));
+    core.endGroup();
+
+    // we can set variable to be used later
+    core.exportVariable('HELLO', 'Hola!');
+    
 } catch (error) {
     core.setFailed(error.message);
 }
