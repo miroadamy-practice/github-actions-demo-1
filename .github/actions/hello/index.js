848d3a8e385cb91c827f78ace2e6b03c1b15a54a
Miro Adamy <miro.adamy@pivotree.com>
2022-08-30 22:31:14 +0200

    feat: exploring github/core

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -5,13 +5,27 @@ const github = require('@actions/github');
 try {
     // throw( new Error('Some simulated error'));
 
+    core.debug('Debug message');
+    core.warning('Warning message');
+
+    core.error('Error Message - it will NOT cause the fail');
+
+
     const name = core.getInput('whom-to-greet');
+
+    core.setSecret(name);   // this will cause the name not being displayed
     console.log(`Hello, ${name}`);
 
     const time = new Date();
     core.setOutput("time", time.toTimeString());
 
+    core.startGroup('Logging Github object');
     console.log(JSON.stringify(github, null, '\t'));
+    core.endGroup();
+
+    // we can set variable to be used later
+    core.exportVariable('HELLO', 'Hola!');
+    
 } catch (error) {
     core.setFailed(error.message);
 }
