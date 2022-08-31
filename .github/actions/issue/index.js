324216746394c70a3b35f1767ab7aede6ecbe09b
Miro Adamy <miro.adamy@pivotree.com>
2022-08-31 21:18:11 +0200

    fix: now trying octokit = github.getOctokit(token);

--- .github/actions/issue/index.js
+++ .github/actions/issue/index.js
@@ -8,7 +8,7 @@ async function run() {
     const body = core.getInput("body");
     const assignees = core.getInput("assignees");
 
-    const octokit = new github.getOctokit(token);
+    const octokit = github.getOctokit(token);
 
     const response = await octokit.issues.create({
       // owner: github.context.repo.owner,
