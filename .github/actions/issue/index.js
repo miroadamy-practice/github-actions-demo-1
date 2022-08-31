4519f5c5b4db45ba5969edb3e7d4329e0b9aaaab
Miro Adamy <miro.adamy@pivotree.com>
2022-08-31 21:08:54 +0200

    fix: new github.Octokit()

--- .github/actions/issue/index.js
+++ .github/actions/issue/index.js
@@ -8,7 +8,7 @@ async function run() {
     const body = core.getInput("body");
     const assignees = core.getInput("assignees");
 
-    const octokit = new github.GitHub(token);
+    const octokit = new github.Octokit(token);
 
     const response = await octokit.issues.create({
       // owner: github.context.repo.owner,
